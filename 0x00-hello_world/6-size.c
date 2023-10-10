f101a0f1cbc504e5184c4ffc3ebf031ed5e60435
teenmaster <ambagwaeugene09@gmail.com>
2023-10-10 07:39:14 -0700

    sizeof

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,7 +2,7 @@
 /**
  * main - prints the size of each data type
  *
- * Description: A C program that prints the size of various types 
+ * Description: A C program that prints the size of various types
  * on the computer it is compiled and run on.
  *
  * Return: Always a 0
