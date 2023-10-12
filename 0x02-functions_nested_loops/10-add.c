965f3bec089de5558cea745755d92f3c3564a7c0
teenmaster <ambagwaeugene09@gmail.com>
2023-10-11 19:29:52 -0700

    addition og=f teo integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - adds two integers
+ *
+ * @a: integer to add to b
+ * @b: integer to add to a
+ *
+ * Return: result
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
