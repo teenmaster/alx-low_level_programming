1a3563600a1f746f822137586e8703bbe4050d91
teenmaster <ambagwaeugene09@gmail.com>
2023-10-12 13:09:03 -0700

    isupper

---
+++ oxo4-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _isupper - checks for uppercase character
+ *
+ * @c - character being checked
+ * Return: 1 if c is uppercase and 0 if otherwise
+ */
+int _isupper(int c)
+{
+	if (c == 'C')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
