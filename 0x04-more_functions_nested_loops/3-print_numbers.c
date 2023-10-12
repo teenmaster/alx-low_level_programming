a8c7d0fd19621e4bc0909dcc04873baff1b742c3
teenmaster <ambagwaeugene09@gmail.com>
2023-10-12 13:52:18 -0700

    prints nos 0 - 9

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,13 +2,13 @@
 
 /**
  * print_numbers - prints 0 to 9 followed by a new line
- * @void: return values will not be included  in the curly braces
+ * eturn: 0
  */
 void print_numbers(void)
 {
 	for (int a = 0; a <= 9; a++)
 	{
-		_putchar(i);
+		_putchar('0' + i);
 	}
 
 	_putchar('\n');
