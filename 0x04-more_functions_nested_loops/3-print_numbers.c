28afd1ab9278ccdd18678afdd62408bfc19034c9
teenmaster <ambagwaeugene09@gmail.com>
2023-10-13 01:40:07 -0700

    print numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_numbers - prints 0 to 9 followed by a new line
---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,6 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints numbers 0 - 9 without 2 and 4
+ * Return: void
+ */
