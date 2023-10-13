a1d74debf7d1fc2996a88c0acbad03dc57303e1b
teenmaster <ambagwaeugene09@gmail.com>
2023-10-13 01:55:54 -0700

    prints 0-9 without 2 and 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,5 +2,16 @@
 
 /**
  * print_most_numbers - prints numbers 0 - 9 without 2 and 4
+ * @i: number to be printed
  * Return: void
  */
+void print_most_numbers(void)
+{
+	int i;
+	
+	while (i == 0 || i ==1 || i == 3 || i >= 5 || i <= 9 )
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
