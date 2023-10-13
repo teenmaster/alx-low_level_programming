07b0ac7ab5f388476f9ef7e8ec3e7ee6dc67570f
teenmaster <ambagwaeugene09@gmail.com>
2023-10-14 01:34:51 +0300

    print diagonal

---
+++ 0x04-more_functions_nested_loops/7-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * nain - checks code
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	print_diagonal(0);
+	print_diagonal(2);
+	print_diagonal(10);
+	print_diagonal(-4);
+	return (0);
+}
