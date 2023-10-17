3a392bd1c95a9e18eb6a8498df3fd979b8b78cec
teenmaster <ambagwaeugene09@gmail.com>
2023-10-17 18:04:03 +0300

    task 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_array - prints n elements of ana array of integers, followed by a new
+ * line and they must be in the order that they are stored in memory
+ *
+ * @a: integer array to be printed
+ * @n: number of elements of the array to be printed
+ *
+ * Return: void
+ */
+void print_array(int *a, int n)
+{
+	int b;
+
+	for (b = 0; b < n; b++)
+	{
+		printf("%d", a[b]);
+		if (b != n - 1)
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+}
