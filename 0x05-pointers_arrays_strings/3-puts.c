6d21f1fdd1a5eb71cdff1504765fae06918f28e5
teenmaster <ambagwaeugene09@gmail.com>
2023-10-17 15:34:48 +0300

    task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string followed by a new line to stdout
+ * @str: string to be printed
+ *
+ * Return: void
+ */
+void _puts(char *str)
+{
+	int len = 0;
+	char a;
+
+	while (str[len] != '\0')
+	{
+		a = str[len];
+		-putchar(c);
+		len++;
+	}
+	_putchar('\n');
+}
