32bb521046c07b42cc18fd878160b870a3b85abd
teenmaster <ambagwaeugene09@gmail.com>
2023-10-17 15:13:36 +0300

    task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: the string whose length is being returned
+ *
+ * Return: string
+ */
+int _strlen(char *s)
+{
+	return *s;
+}
